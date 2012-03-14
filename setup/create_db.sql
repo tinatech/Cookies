42d8a829a97d98126cd6a8b493cb6e6166779543
Christoffer Hallstensen <christoffer@netcrawlr.net>
2012-03-14 19:41:31 +0100

    New loadenv.php and added pby (processed by in ordr) create_db.sql order is changed to ordr because of reserved word by mysql

--- setup/create_db.sql
+++ setup/create_db.sql
@@ -121,15 +121,16 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `Webshop`.`order`
+-- Table `Webshop`.`ordr`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `Webshop`.`order` ;
+DROP TABLE IF EXISTS `Webshop`.`ordr` ;
 
-CREATE  TABLE IF NOT EXISTS `Webshop`.`order` (
+CREATE  TABLE IF NOT EXISTS `Webshop`.`ordr` (
   `orderID` INT NOT NULL AUTO_INCREMENT ,
   `uid` INT NULL ,
   `status` INT(1) NULL ,
   `time` DATETIME NOT NULL ,
+  `pby` INT ,
   PRIMARY KEY (`orderID`) ,
   INDEX `FK_order_uid` (`uid` ASC) ,
   CONSTRAINT `FK_order_uid`
@@ -157,7 +158,7 @@ CREATE  TABLE IF NOT EXISTS `Webshop`.`orders` (
     ON UPDATE NO ACTION,
   CONSTRAINT `FK_orders_orderID`
     FOREIGN KEY (`orderID` )
-    REFERENCES `Webshop`.`order` (`orderID` )
+    REFERENCES `Webshop`.`ordr` (`orderID` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -184,7 +185,7 @@ CREATE  TABLE IF NOT EXISTS `Webshop`.`orderlines` (
     ON UPDATE NO ACTION,
   CONSTRAINT `FK_orderlines_orderID`
     FOREIGN KEY (`orderID` )
-    REFERENCES `Webshop`.`order` (`orderID` )
+    REFERENCES `Webshop`.`ordr` (`orderID` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
