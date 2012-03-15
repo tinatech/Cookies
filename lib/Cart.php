2ab09d287ecdfd9f7f1b7bb6f34284d7cadcae06
Christoffer Hallstensen <christoffer@netcrawlr.net>
2012-03-15 20:06:10 +0100

    Cart()

--- lib/Cart.php
+++ lib/Cart.php
@@ -15,19 +15,39 @@ class Cart () {
 	
 	var $expire = 3600;
 	var $del_cookie = -1;
+	$dbconn = NULL;
 
 	function __construct() {
 		session_start();
-		$_SESSION['cart'] = Array items;
+		$_SESSION['cart'] = array();
+		$this->dbconn = new Database();
 	}
 
-	function addItem($itemID) {
-		if(isSet($_SESSION['cart']) {
-		}
+	function addItem($itemID, $qty) {
+		$sql = 'SELECT (price) FROM item WHERE itemID='$itemID' LIMIT 0,1';
+		$result = $dbconn->dbQuery($sql);
+		$price = $result[0][0];
+
+		$item = array(
+			$itemID => $itemID,
+			$ItemQty => $qty,
+	       		$itemPrice => $price);
+
+		array_push($_SESSION['cart'], $item);
+		print_r($_SESSION['cart']);
 	}
 
 	function rmItem($itemID) {
+	
 	}
+
+	function calcTotalPrice () {
+	
+	}
+
+	function calcTotalItems () {
+	
+	}	
 
 	function checkout() {
 	
